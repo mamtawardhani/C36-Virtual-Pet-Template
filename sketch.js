fa61d5af7b0bf03d8ba12f6b8722124347a8f22f
mamtawardhani <53176352+mamtawardhani@users.noreply.github.com>
2021-07-06 22:40:54 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -56,17 +56,7 @@ function draw() {
  
   fill(255,255,254);
   textSize(15);
- 
-//code to display the feed time
-  if(lastFed>=12){
-    text("Last Feed : "+ lastFed%12 + " PM", 350,30);
-  }else if(lastFed==0){
-     text("Last Feed : 12 AM",350,30);
-  }else{
-     text("Last Feed : "+ lastFed + " AM", 350,30);
-  }
-
- 
+  
   drawSprites();
 }
 
